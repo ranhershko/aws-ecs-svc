edc9d9b041f5744c970c9760e7af2d9059507c81
Ran Hershko <ran.hershko@gmail.com>
2020-09-20 17:36:31 +0300

    deleted:    README.md

--- modules/alb/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "listener_https_arn" {
-  description = "The ARN of the HTTPS ALB Listener that can be used to add rules"
-  value       = "${module.alb.alb_listener_https_arn}"
-}
-
-output "target_group_arn" {
-  description = "ARN of the target group"
-  value       = "${module.alb.target_group_arn}"
-}
