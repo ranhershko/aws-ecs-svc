4fc25ae82dd6851e83cacbd36343f2be352176ff
Ran Hershko <ran.hershko@gmail.com>
2020-09-22 15:45:03 +0300

    move terraform remote state configuration to ansible

--- terraform/vpc-ecs-n-ecr-environment/create-vpc-n-ecs-backend.tf
+++ terraform/vpc-ecs-n-ecr-environment/create-vpc-n-ecs-backend.tf
@@ -1,11 +1,11 @@
 terraform {
   required_version = ">= 0.12.0"
-  backend "s3" {
-    encrypt = true
-    bucket  = "awsecs-terraform-remote-state"
-    key     = "vpc-n-ecs-terraform.tfstate"
-    region  = "us-east-1"
+  backend "s3" {}
+    #encrypt = true
+    #bucket  = "awsecs-terraform-remote-state"
+    #key     = "vpc-n-ecs-terraform.tfstate"
+    #region  = "us-east-1"
 
-    dynamodb_table = "vpc-n-ecs-terraform-remote-lock"
- } 
+    #dynamodb_table = "vpc-n-ecs-terraform-remote-lock"
+ #} 
 }
