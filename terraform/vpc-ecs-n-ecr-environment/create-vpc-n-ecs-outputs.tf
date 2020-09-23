1456b34bfc3d5c1bc4908a5d0f6c1d951e9d05d9
Ran Hershko <ran.hershko@gmail.com>
2020-09-23 03:20:08 +0300

    Add codecommit_repo_url output

--- terraform/vpc-ecs-n-ecr-environment/create-vpc-n-ecs-outputs.tf
+++ terraform/vpc-ecs-n-ecr-environment/create-vpc-n-ecs-outputs.tf
@@ -9,3 +9,7 @@ output "lb_target_group_arn" {
 output "ecs_cluster_id" {
   value = module.ecs_cluster.cluster_id
 }
+
+output "codecommit_repo_url" {
+  value = aws_codecommit_repository".repo.clone_url_ssh
+}
